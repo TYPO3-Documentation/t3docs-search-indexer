f54611dc9764e7b05b9a5af7690e0e806443e275
Tymoteusz Motylewski <t.motylewski@gmail.com>
2021-03-29 21:22:38 +0200

    Use newer (not deprecated Route annotaton

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 
 use App\Dto\SearchDemand;
 use App\Repository\ElasticRepository;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
