51eabd480997178f1ab36ae5ba767b2e56c5a76b
Marcin Sągol <m.sagol@macopedia.com>
2024-03-22 12:21:46 +0100

    [TASK] Allow searching only inside single manual scope
    This commit follows up on the changes made in the Rendering Guides repository,
    where a new option for searching within a single manual scope was introduced.
    
    These changes implement the same functionality for the search results page:
    
    - The search form has been updated to display a list of search scopes (All | Current).
    - The selected scope is now passed as a filter to the Elasticsearch query to match against manual_slug.
    - If a scope is selected, facets are hidden on the search results page.
    - Styles have been updated to the latest version.
    - Several improvements have been made to the templates.

--- src/Dto/SearchDemand.php
+++ src/Dto/SearchDemand.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 readonly class SearchDemand
 {
-    public function __construct(private string $query, private int $page, private array $filters)
+    public function __construct(private string $query, private string $scope, private int $page, private array $filters)
     {
     }
 
@@ -34,12 +34,23 @@ readonly class SearchDemand
         $page = (int)$request->query->get('page', '1');
         $query = $request->query->get('q', '');
 
-        return new self($query, max($page, 1), $filters);
+        // scope points to given manual version and language
+        $scope = trim(htmlspecialchars(strip_tags((string)$request->query->get('scope'))), '/');
+        if ($scope) {
+            $filters['manual_slug'] = [$scope];
+        }
+
+        return new self($query, $scope, max($page, 1), $filters);
     }
 
     public function getQuery(): string
     {
         return $this->query;
+    }
+
+    public function getScope(): string
+    {
+        return $this->scope;
     }
 
     public function getPage(): int
