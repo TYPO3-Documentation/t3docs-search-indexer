8684715534dacdf6d718efc4e3c0765b96b1393f
Oskar Dydo <oskar.dydo@gmail.com>
2024-10-29 09:10:02 +0100

    [TASK] Describe methods generateLinkWithout and generateLinkWith used in twig templates

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -3,9 +3,11 @@
 namespace App\Twig;
 
 use App\Config\Labels;
+use App\Dto\SearchDemand;
 use App\Helper\VersionFilter;
 use App\Helper\VersionSorter;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Twig\Environment;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
@@ -15,6 +17,7 @@ class AppExtension extends AbstractExtension
     public function __construct(
         private readonly ParameterBagInterface $parameterBag,
         private readonly Environment $twigEnvironment,
+        private readonly UrlGeneratorInterface $urlGenerator,
     ) {
     }
 
@@ -24,6 +27,8 @@ class AppExtension extends AbstractExtension
             new TwigFunction('render_assets', $this->renderAssets(...)),
             new TwigFunction('render_single_asset', $this->renderSingleAsset(...)),
             new TwigFunction('aggregationBucket', $this->aggregationBucket(...), ['is_safe' => ['html']]),
+            new TwigFunction('generateLinkWithout', $this->generateLinkWithout(...)),
+            new TwigFunction('generateLinkWith', $this->generateLinkWith(...)),
             new TwigFunction('getLabelForFilter', $this->getLabelForFilter(...)),
             new TwigFunction('sortVersions', VersionSorter::sortVersions(...)),
             new TwigFunction('filterVersions', VersionFilter::filterVersions(...)),
@@ -93,6 +98,70 @@ class AppExtension extends AbstractExtension
         }
 
         return $in;
+    }
+
+    /**
+     * Generates a frontend link to be used in Twig with the current demand filters, excluding a specific value
+     * in the specified filter
+     *
+     * Example:
+     *  Suppose we have the following query parameters:
+     *   - filters[vendor]=typo3
+     *   - filters[sversion]=main
+     *
+     *  If we want to generate a link that excludes vendor=typo3, we can use this method by passing:
+     *   - $key = vendor
+     *   - $value = typo3
+     *
+     * This will generate a link that includes only filters[sversion]=main.
+     *
+     * @param SearchDemand $demand current search demand object
+     * @param string $key name of the filter (e.g. vendor in filters[vendor]=typo3)
+     * @param mixed $value value of the filter (e.g. typo3 in filters[vendor]=typo3)
+     * @param bool $removeQuery whether to keep or remove 'q' from query parameters when generating the link
+     * @return string generated link without specific query param if exists
+     */
+    private function generateLinkWithout(SearchDemand $demand, string $key, mixed $value, bool $removeQuery = true): string
+    {
+        $filters['filters'] = $demand->withFilterValueForLinkGeneration($key, $value);
+
+        if ($removeQuery === false) {
+            $filters['q'] = $demand->getQuery();
+        }
+
+        return $this->urlGenerator->generate('search-with-suggest', $filters);
+    }
+
+
+    /**
+     * Generates a frontend link to be used in Twig with the current demand filters, including a specific value
+     * in the specified filter
+     *
+     * Example:
+     *  Suppose we have the following query parameter:
+     *   - filters[sversion]=main
+     *
+     *  If we want to generate a link that includes vendor = typo3, we can use this method by passing:
+     *   - $key = vendor
+     *   - $value = typo3
+     *
+     * This will generate a link that includes both filters[sversion]=main and filters[vendor]=typo3
+     *
+     * @param SearchDemand $demand current search demand object
+     * @param string $key name of the filter (e.g. vendor in filters[vendor]=typo3)
+     * @param mixed $value value of the filter (e.g. typo3 to add to the query string)
+     * @param bool $addQuery whether to include or exclude 'q' from query parameters when generating the link
+     * @return string generates a link with a specific query parameter. If an invalid filter is provided, the new filters[$key]=$value will be ignored
+     */
+    private function generateLinkWith(SearchDemand $demand, string $key, mixed $value, bool $removeQuery = true): string
+    {
+        $filters['filters'] = $demand->withoutFilterValueForLinkGeneration($key, $value);
+
+        if ($removeQuery === false) {
+            $filters['q'] = $demand->getQuery();
+        }
+
+        return $this->urlGenerator->generate('search-with-suggest', $filters);
     }
 
     private function getLabelForFilter(string $filter): string
